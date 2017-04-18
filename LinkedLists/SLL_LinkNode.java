959fbcb231c0473099d32e61e52e3c45f6b2432c
Pavithra Raghavan <pavithraraghavan@Pavithras-MacBook-Pro.local>
2017-04-18 21:22:44 +0530

    adding singly LL node

---
+++ LinkedLists/SLL_LinkNode.java
@@ -0,0 +1,29 @@
+package Implementation;
+
+public class SLL_LinkNode {
+//Declaring elements for Singly-LL Link node	
+	private int data;
+	private SLL_LinkNode next;
+
+//Constructor
+	public SLL_LinkNode(int item){
+		this.setData(item);
+		this.setNext(null);
+	}
+	
+//Methods to set data and next pointer
+	public void setData(int item){
+		this.data = item;
+	}
+	public void setNext(SLL_LinkNode ptr){
+		this.next = ptr;
+	}
+	
+//Methods to return data and next pointer for this object
+	public int getData(){
+		return this.data;
+	}
+	public SLL_LinkNode getNext(){
+		return this.next;
+	}
+}
