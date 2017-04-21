181a0fed33e3698395bc2d7947e3ae45d904659b
Pavithra Raghavan <pavithraraghavan@Pavithras-MacBook-Pro.local>
2017-04-21 08:38:10 +0530

    recursive method for reversing a list

--- LinkedLists/ReverseLL.java
+++ LinkedLists/ReverseLL.java
@@ -1,21 +1,35 @@
 package Algorithms;
+
 import Implementation.*;
 
 public class ReverseLL {
-	public SLL_LinkNode reverse_LL(SLL_LinkNode head) {
+	private SLL_LinkNode head;
+
+	public SLL_LinkNode Iterative_ReverseLL(SLL_LinkNode head) {
 		/*
-		 * pre: head node of the Singly LL 
-		 * post: head node should be the last
-		 *       node pointing to null and new head node is the last node
+		 * 1 2 3 4 5 null pre: head node of the Singly LL post: head node should
+		 * be the last node pointing to null and new head node is the last node
 		 */
 		SLL_LinkNode prev = null, curr = head, next = head;
 		while (next != null) {
 			next = curr.getNext();
 			curr.setNext(prev);
-			prev=curr;
+			prev = curr;
 			if (next != null)
 				curr = next;
 		}
 		return curr;
+	}
+
+	public SLL_LinkNode Recursive_ReverseLL(SLL_LinkNode curr, SLL_LinkNode prev) {
+		if (curr.getNext() == null) {
+			head = curr;
+			curr.setNext(prev);
+			return null;
+		}
+		SLL_LinkNode next = curr.getNext();
+		curr.setNext(prev);
+		Recursive_ReverseLL(next, curr);
+		return head;
 	}
 }
