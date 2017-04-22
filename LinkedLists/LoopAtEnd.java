4b511938948a2eb147ab8326b4984ff62800cf8f
Pavithra Raghavan <pavithraraghavan@Pavithras-MacBook-Pro.local>
2017-04-22 13:56:50 +0530

    remove loop at the end of list

--- LinkedLists/LoopAtEnd.java
+++ LinkedLists/LoopAtEnd.java
@@ -12,14 +12,27 @@ package Algorithms;
 import Implementation.*;
 
 public class LoopAtEnd {
+	private SLL_LinkNode meetingNode;
+
 	public boolean checkForLoop(SLL_LinkNode slow) {
 		SLL_LinkNode fast = slow;
 		while (fast != null && fast.getNext() != null) {
 			fast = fast.getNext().getNext();
 			slow = slow.getNext();
-			if (fast == slow)
+			if (fast == slow) {
+				meetingNode = slow;
 				return true;
+			}
 		}
 		return false;
+	}
+
+	public void removeLoop(SLL_LinkNode ptr1) {
+		SLL_LinkNode ptr2 = meetingNode;
+		while (ptr1.getNext() != ptr2.getNext()) {
+			ptr1 = ptr1.getNext();
+			ptr2 = ptr2.getNext();
+		}
+		ptr2.setNext(null);
 	}
 }
