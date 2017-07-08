fddb9e0dff63ecc42f670efc8f3c24b77b8c2200
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-08 21:44:05 +0530

    isSorted

--- LinkedLists/SeqSearch.java
+++ LinkedLists/SeqSearch.java
@@ -2,28 +2,40 @@ package Algorithms;
 
 public class SeqSearch {
 	public int seq_search(int[] a, int x) {
-		for(int i=0; i<a.length;i++) {
-			if(a[i]==x)
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] == x)
 				return i;
 		}
 		return -1;
 	}
-	public int binary_search(int[] a, int x){
-		int low=0, high=a.length,mid;
-		while(low<high) {
-			mid=(low+high)/2;
-			if(a[mid]==x)
+
+	public int binary_search(int[] a, int x) {
+		int low = 0, high = a.length, mid;
+		while (low < high) {
+			mid = (low + high) / 2;
+			if (a[mid] == x)
 				return mid;
 			else if (a[mid] < x)
-				low=mid+1;
+				low = mid + 1;
 			else
-				high=mid;
+				high = mid;
 		}
 		return -1;
 	}
+
+	public boolean isSorted(int[] a) {
+		for (int i = 1; i < a.length; i++) {
+			if (a.length < 2)
+				return true;
+			if (a[i] < a[i - 1])
+				return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
-		int[] a = { 1,2,3,4,5,6,7};
-		SeqSearch obj = new SeqSearch(); 
-		System.out.println("i = " + obj.binary_search(a,1));
+		int[] a = { 1, 2, 3, 4, 5, 7, 7 };
+		SeqSearch obj = new SeqSearch();
+		System.out.println("i = " + obj.isSorted(a));
 	}
 }
