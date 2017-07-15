5324fdfe119da975b51b22c3ff6cbc49ad152d40
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-15 21:33:24 +0530

    Josephus Circle edit - space reduction

--- LinkedLists/JosephusCircle.java
+++ LinkedLists/JosephusCircle.java
@@ -4,18 +4,18 @@ import Implementation.*;
 
 public class JosephusCircle {
 	public SLL_LinkNode josephusCircle(SLL_LinkNode head, int M) {
-		SLL_LinkNode ptr1 = head, ptr2;
-		if (ptr1.getNext() != head)
-			ptr2 = head.getNext();
+		SLL_LinkNode ptr1 = head;
+		if (head.getNext() != head)
+			ptr1 = head.getNext();
 		else
 			return head;
 		
 		while(ptr1.getNext()!=ptr1) {
-			for(int i=1; i<M; i++) {
+			for(int i=1; i<M-1; i++) {
 				ptr1=ptr1.getNext();
-				ptr2=ptr1.getNext();
 			}
-			ptr1.setNext(ptr2.getNext());
+			ptr1.setNext(ptr1.getNext().getNext());
+			ptr1=ptr1.getNext();
 		}
 		return ptr1;
 	}
