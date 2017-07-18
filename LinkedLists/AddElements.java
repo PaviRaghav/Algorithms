2767aa4c0dfd6ced328ef055160b2300e8989fa6
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-18 21:40:05 +0530

    add individual elements of 2 LL

---
+++ LinkedLists/AddElements.java
@@ -0,0 +1,14 @@
+package Algorithms;
+/**
+ * This class is to add the individual elements of two LL's which are in reverse order.
+ * LL1: 5-->7-->6-->null
+ * LL2: 2-->3-->3-->null
+ * output: 9-->0-->8-->null
+ * 
+ * summation in reverse order, but with single digit. Carry over is added to the previous node.
+ * @author pavithraraghavan
+ *
+ */
+public class AddElements {
+
+}
