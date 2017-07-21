812e2430f2ae0015c16366d5a2d0071c2f2b7ef5
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-21 21:55:20 +0530

    Stacks

--- Stacks/Stacks.java
+++ Stacks/Stacks.java
@@ -1,9 +1,9 @@
 package Implementation;
 
-public interface Stacks {
-	public Object top();
-	public Object pop();
-	public boolean push(Object item);
+public interface Stacks<E> {
+	public E top();
+	public E pop();
+	public boolean push(E item);
 	public int size();
 	public boolean isFull();
 	public boolean isEmpty();
