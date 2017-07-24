c57d2b6db84fdb61fb4f635cd8e239f6de489689
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-24 21:03:04 +0530

    Stack using LL

---
+++ Stacks/Stack_LL.java
@@ -0,0 +1,11 @@
+package Implementation;
+
+public class Stack_LL<E> implements Stacks<E> {
+	private int top, maxSize;
+	public E top() throws Exception {}
+	public E pop() throws Exception {}
+	public void push(E item) throws Exception {}
+	public int size() {}
+	public boolean isFull() {}
+	public boolean isEmpty() {}
+}
