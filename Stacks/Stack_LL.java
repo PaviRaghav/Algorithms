4d7a553304545d84af5569e3b7ed520f294ac8c9
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-24 21:20:54 +0530

    push

--- Stacks/Stack_LL.java
+++ Stacks/Stack_LL.java
@@ -1,37 +1,47 @@
 package Implementation;
 
-public class Stack_LL  {
+public class Stack_LL {
 	private SLL_List StackLL;
 	private int top;
+
 	public Stack_LL() {
 		this.StackLL = new SLL_List();
-		this.top=0;
+		this.top = 0;
 	}
+
 	public int top() throws Exception {
 		if (!isEmpty())
 			return StackLL.getHead().getData();
 		else
 			throw new Exception("Stack is empty");
 	}
+
 	public int pop() throws Exception {
 		if (!isEmpty()) {
-			SLL_LinkNode temp=StackLL.getHead();
+			SLL_LinkNode temp = StackLL.getHead();
 			StackLL.setHead(temp.getNext());
 			temp.setNext(null);
 			this.top--;
 			return temp.getData();
-		}
-		else
+		} else
 			throw new Exception("Stack is empty");
 	}
-	public void push(int item) throws Exception {}
+
+	public void push(int item) throws Exception {
+		SLL_LinkNode temp = new SLL_LinkNode(item);
+		temp.setNext(this.StackLL.getHead());
+		this.StackLL.setHead(temp);
+		this.top++;
+	}
+
 	public int size() {
 		return this.top;
-		}
+	}
+
 	public boolean isEmpty() {
 		if (this.top == 0)
 			return true;
 		else
 			return false;
-		}
+	}
 }
