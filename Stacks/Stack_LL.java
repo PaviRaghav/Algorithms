6bffcfd1983af35a6ae8469799048cd28f61dcbe
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-24 21:08:50 +0530

    constructor

--- Stacks/Stack_LL.java
+++ Stacks/Stack_LL.java
@@ -1,7 +1,12 @@
 package Implementation;
 
 public class Stack_LL<E> implements Stacks<E> {
-	private int top, maxSize;
+	private SLL_List StackLL;
+	private int top;
+	public Stack_LL() {
+		this.StackLL = new SLL_List();
+		this.top=-1;
+	}
 	public E top() throws Exception {}
 	public E pop() throws Exception {}
 	public void push(E item) throws Exception {}
