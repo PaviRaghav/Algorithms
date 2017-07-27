1687daa1e365f661979a4a57d85d54f70fb8004a
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-27 22:51:49 +0530

    inToPost method

--- Stacks/InfixToPostfix.java
+++ Stacks/InfixToPostfix.java
@@ -1,5 +1,7 @@
 package Algorithms;
 
+import Implementation.*;
+
 /**
  * This class is to convert infix to postfix----------------------------
  * (A+B)*(C-D) --> AB+CD-*
@@ -8,5 +10,13 @@ package Algorithms;
  *
  */
 public class InfixToPostfix {
-
+	public String inToPost(String input) {
+		String out;
+		Stack_DynArray<String> stk = new Stack_DynArray<String>();
+		for (int i = 0; i < input.length(); i++) {
+			char temp = input.charAt(i);
+			
+		}
+		return out;
+	}
 }
