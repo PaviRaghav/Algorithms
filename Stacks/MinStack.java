06269aea966ab235f7e6419e925fac66d6716c89
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-30 21:57:21 +0530

    auxillary stack

--- Stacks/MinStack.java
+++ Stacks/MinStack.java
@@ -1,10 +1,26 @@
 package Algorithms;
 
+import Implementation.*;
+
 /**
  * This class is to find the minimum element in the stack with O(1) time
+ * 
  * @author pavithraraghavan
  *
  */
 public class MinStack {
+	private Stack_LL stk_main = new Stack_LL();
+	private Stack_LL stk_aux = new Stack_LL();
 
+	public int getMin() {
+
+	}
+
+	public void push(int item) {
+		try {
+			stk_main.push(item);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
