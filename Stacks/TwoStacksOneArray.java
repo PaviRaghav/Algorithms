68d9e4a54edce0a4829269047ecdd6a0d40fca53
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-31 21:41:56 +0530

    isempty

--- Stacks/TwoStacksOneArray.java
+++ Stacks/TwoStacksOneArray.java
@@ -2,10 +2,32 @@ package Algorithms;
 
 /**
  * This class is to implement 2 stacks using a single array
+ * 
  * @author pavithraraghavan
  *
  */
 public class TwoStacksOneArray {
-	
+	private int[] stk;
+	int top1, top2, len;
 
+	public TwoStacksOneArray(int size) {
+		len = size * 2;
+		this.stk = new int[len];
+		this.top1 = -1;
+		this.top2 = len;
+	}
+
+	public boolean stk1_isEmpty() {
+		if (top1 == -1)
+			return true;
+		else
+			return false;
+	}
+
+	public boolean stk2_isEmpty() {
+		if (top1 == this.len)
+			return true;
+		else
+			return false;
+	}
 }
