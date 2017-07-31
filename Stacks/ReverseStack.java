003a22600c00ed9b87779eaa502bb2130cc8e2ad
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-31 20:59:07 +0530

    reverse stack using push and pop

--- Stacks/ReverseStack.java
+++ Stacks/ReverseStack.java
@@ -1,5 +1,7 @@
 package Algorithms;
 
+import Implementation.*;
+
 /**
  * This class is to reverse a stack by just using push and pop
  * 
@@ -7,5 +9,29 @@ package Algorithms;
  *
  */
 public class ReverseStack {
+	public static void reverse(Stack_LL stk) {
+		try {
+			if (stk.isEmpty())
+				return;
+			int temp = stk.pop();
+			reverse(stk);
+			insertAtBottom(stk, temp);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
+	public static void insertAtBottom(Stack_LL stk, int data) {
+		try {
+			if (stk.isEmpty()) {
+				stk.push(data);
+				return;
+			}
+			int temp = stk.pop();
+			insertAtBottom(stk, data);
+			stk.push(temp);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
