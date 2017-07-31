43c6b3e02cd1c7f3f28f2a2dfd4f825023b2c87a
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-07-31 22:07:10 +0530

    2 stacks in one array

--- Stacks/TwoStacksOneArray.java
+++ Stacks/TwoStacksOneArray.java
@@ -11,6 +11,8 @@ public class TwoStacksOneArray {
 	int top1, top2, len;
 
 	public TwoStacksOneArray(int size) {
+		if (size < 2)
+			throw new IllegalStateException("Size less than 2 is not allowed");
 		len = size * 2;
 		this.stk = new int[len];
 		this.top1 = -1;
