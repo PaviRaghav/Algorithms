6c729808f4875929772918455458337ccec4bb00
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-08-01 19:13:50 +0530

    span O(n2)

--- Stacks/Span.java
+++ Stacks/Span.java
@@ -9,5 +9,17 @@ import Implementation.*;
  *
  */
 public class Span {
-
+	public int[] findSpan(int[] inputArray) {
+		int[] spans = new int[inputArray.length];
+		for (int i = 0; i < inputArray.length; i++) {
+			int span = 1;
+			int j = i - 1;
+			while (j >= 0 && inputArray[j] <= inputArray[j + 1]) {
+				span++;
+				j--;
+			}
+			spans[i] = span;
+		}
+		return spans;
+	}
 }
