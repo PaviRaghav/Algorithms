1c98ad54f288e5838c3940e246d4bb8e0115bbd9
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-08-01 21:13:54 +0530

    span O(n)

--- Stacks/Span.java
+++ Stacks/Span.java
@@ -22,4 +22,52 @@ public class Span {
 		}
 		return spans;
 	}
+
+	public int[] improvedFindSpan(int[] inputArray) {
+		Stack_LL stk = new Stack_LL();
+		int[] spans = new int[inputArray.length];
+		int p = -1;
+		try {
+			for (int i = 0; i < inputArray.length; i++) {
+				if (p != -1 && inputArray[i] > inputArray[p])
+					stk.pop();
+				if (stk.isEmpty())
+					p = -1;
+				else
+					p = stk.top();
+				spans[i] = i - p;
+				stk.push(i);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		for (int i = 0; i < inputArray.length; i++)
+			System.out.println(spans[i] + " ");
+		return spans;
+	}
+
+	public static void main(String[] args) {
+		int[] inputArray = { 6, 3, 4, 5, 2 };
+		Stack_LL stk = new Stack_LL();
+		int[] spans = new int[inputArray.length];
+		int p = 0;
+		try {
+			for (int i = 0; i < inputArray.length; i++) {
+				if (!stk.isEmpty()) {
+					if (inputArray[i] > inputArray[stk.top()])
+						stk.pop();
+				}
+				if (stk.isEmpty())
+					p = -1;
+				else
+					p = stk.top();
+				spans[i] = i - p;
+				stk.push(i);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		for (int i = 0; i < inputArray.length; i++)
+			System.out.println(spans[i] + " ");
+	}
 }
