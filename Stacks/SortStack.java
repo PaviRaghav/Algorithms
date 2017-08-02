7c4f5c1af559a634170452c6375c09ba66c873f2
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-08-02 23:48:54 +0530

    sort stack

--- Stacks/SortStack.java
+++ Stacks/SortStack.java
@@ -10,5 +10,18 @@ import Implementation.*;
  */
 
 public class SortStack {
-	
+	public Stack_LL seqSort(Stack_LL stk) {
+		Stack_LL rstk = new Stack_LL();
+		try {
+			while (!stk.isEmpty()) {
+				int tmp = stk.pop();
+				while (!rstk.isEmpty() && rstk.top() > tmp)
+					stk.push(rstk.pop());
+				rstk.push(tmp);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return rstk;
+	}
 }
