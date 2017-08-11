a2541c695e356c68f5576edcb3555d502c0a2d2c
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-08-11 21:48:46 +0530

    inserting into binary tree

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,54 @@
+package Implementation;
+
+public class BinaryTree {
+	private BinaryTreeNode root;
+
+	public BinaryTree() {
+		root = null;
+	}
+
+	public void insert(int data) {
+		if (this.root == null)
+			root = new BinaryTreeNode(data);
+		else
+			insert(this.root, data);
+	}
+
+	public BinaryTreeNode insert(BinaryTreeNode node, int data) {
+		if (node == null)
+			node = new BinaryTreeNode(data);
+		else {
+			if (node.getData() < data)
+				node.setRight(insert(node.getRight(), data));
+			else
+				node.setLeft(insert(node.getLeft(), data));
+		}
+		return node;
+	}
+
+	public void insert_ite(int data) {
+		BinaryTreeNode newOne = new BinaryTreeNode(data);
+		if (this.root == null)
+			this.root = newOne;
+		else {
+			BinaryTreeNode temp = this.root;
+			while (temp != null) {
+				if (temp.getData() > data) {
+					if (temp.getLeft() != null)
+						temp = temp.getLeft();
+					else {
+						temp.setLeft(newOne);
+						break;
+					}
+				} else {
+					if (temp.getRight() != null)
+						temp = temp.getRight();
+					else {
+						temp.setRight(newOne);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
