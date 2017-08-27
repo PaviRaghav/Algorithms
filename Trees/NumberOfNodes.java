4ffaaaa1ff2f90eae475496d66ebd6945c8ab987
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-08-27 23:07:10 +0530

    number of full nodes

--- Trees/NumberOfNodes.java
+++ Trees/NumberOfNodes.java
@@ -20,4 +20,22 @@ public class NumberOfNodes {
 		}
 		return count;
 	}
+	
+	public static int noOfFullNodes(BinaryTreeNode root) {
+		if (root == null)
+			return 0;
+		int count = 0;
+		Q_DynArray<BinaryTreeNode> q = new Q_DynArray<BinaryTreeNode>();
+		q.Enqueue(root);
+		while (!q.isEmpty()) {
+			BinaryTreeNode temp = q.Dequeue();
+			if (temp.getLeft() != null && temp.getRight() != null)
+				count++;
+			if (temp.getLeft() != null)
+				q.Enqueue(temp.getLeft());
+			if (temp.getRight() != null)
+				q.Enqueue(temp.getRight());
+		}
+		return count;
+	}
 }
