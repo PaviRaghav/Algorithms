819f5f1781f95f634dcd8dc30173874d9dc29abc
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-08-28 23:32:03 +0530

    diameter

--- Trees/NumberOfNodes.java
+++ Trees/NumberOfNodes.java
@@ -20,7 +20,7 @@ public class NumberOfNodes {
 		}
 		return count;
 	}
-	
+
 	public static int noOfFullNodes(BinaryTreeNode root) {
 		if (root == null)
 			return 0;
@@ -37,5 +37,37 @@ public class NumberOfNodes {
 				q.Enqueue(temp.getRight());
 		}
 		return count;
+	}
+
+	public static int diameterOfTree(BinaryTreeNode root) {
+		if (root == null)
+			return 0;
+		int dia = 0;
+		Q_DynArray<BinaryTreeNode> q = new Q_DynArray<BinaryTreeNode>();
+		q.Enqueue(root);
+		while(!q.isEmpty()){
+			BinaryTreeNode temp = q.Dequeue();
+			if(temp!=null){
+				if(temp.getLeft()==null && temp.getRight()==null) q.Enqueue(null);
+				if(temp.getLeft()!=null) q.Enqueue(temp.getLeft());
+				if(temp.getRight()!=null) q.Enqueue(temp.getRight());
+			}
+				
+			
+		}
+		return dia;
+	}
+
+	public static void main(String[] args) {
+		BinaryTreeNode root = new BinaryTreeNode(1);
+		root.left = new BinaryTreeNode(2);
+		root.right = new BinaryTreeNode(3);
+		root.left.left = new BinaryTreeNode(4);
+		root.left.right = new BinaryTreeNode(4);
+		root.right.left = new BinaryTreeNode(5);
+		root.right.right = new BinaryTreeNode(6);
+		root.right.right.right = new BinaryTreeNode(7);
+		// levelOrderTraversal(root);
+		System.out.println(noOfFullNodes(root));
 	}
 }
