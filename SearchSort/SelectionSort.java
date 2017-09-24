b0df5bfb9a78dc3f9054009ec8ec40b45cefddf4
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-09-23 22:35:51 -0400

    selection Sort

--- SearchSort/SelectionSort.java
+++ SearchSort/SelectionSort.java
@@ -4,15 +4,13 @@ public class SelectionSort {
 	public int[] selectionSort(int[] A) {
 		int tempIndex, temp;
 		for (int i = 0; i < A.length - 1; i++) {
-			temp = A[i];
 			tempIndex = i;
-			for (int j = i; j < A.length; j++) {
-				if (A[j] < temp) {
-					temp = A[j];
+			for (int j = i+1; j < A.length; j++) {
+				if (A[j] < A[tempIndex]) 
 					tempIndex = j;
-				}
 			}
 			if (tempIndex != i) {
+				temp = A[tempIndex];
 				A[tempIndex] = A[i];
 				A[i] = temp;
 			}
