fb9d9054ef88a01826994fee82c57c3586f3159c
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-02 00:02:30 -0400

    print all paths from root

--- Trees/PrintTreePath.java
+++ Trees/PrintTreePath.java
@@ -6,15 +6,19 @@ public class PrintTreePath {
 	public static void printPaths(BinaryTreeNode root, int[] path, int pathLen) {
 		if (root == null)
 			return;
-		path[pathLen]=root.getData();
+		path[pathLen] = root.getData();
 		pathLen++;
-		if(root.getLeft()==null && root.getRight()==null)
-			printArray(path,pathLen);
-		else {printPaths(root.getLeft(),path,pathLen);
-		printPaths(root.getRight(),path,pathLen);}
+		if (root.getLeft() == null && root.getRight() == null)
+			printArray(path, pathLen);
+		else {
+			printPaths(root.getLeft(), path, pathLen);
+			printPaths(root.getRight(), path, pathLen);
+		}
 	}
-	
-	private void printArray(int[] ints, int len) {
-		
+
+	private static void printArray(int[] ints, int len) {
+		for (int i = 0; i < len; i++)
+			System.out.println(ints[i] + " ");
+		System.out.println();
 	}
 }
