813f259046b5bb2f411d4b040e190bc6edc91ab4
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-02 22:13:58 -0400

    check if a sum exists in a tree path

---
+++ Trees/SumInPath.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+import Implementation.BinaryTreeNode;
+
+public class SumInPath {
+	public boolean hasPathSum(BinaryTreeNode root, int sum) {
+		if (root == null)
+			return false;
+		if (root.left == null && root.right == null && root.data == sum)
+			return true;
+		else
+			return hasPathSum(root.left, sum - root.data) || hasPathSum(root.right, sum - root.data);
+	}
+}
