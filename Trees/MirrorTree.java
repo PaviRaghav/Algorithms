c91e3666655797106deed38b3386f81952a347f2
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-03 16:11:40 -0400

    check if 2 trees are mirror images of each other

--- Trees/MirrorTree.java
+++ Trees/MirrorTree.java
@@ -13,4 +13,15 @@ public class MirrorTree {
 		}
 		return root;
 	}
+
+	public static boolean checkIfMirror(BinaryTreeNode root1, BinaryTreeNode root2) {
+		if (root1 == null && root2 == null)
+			return true;
+		if ((root1 == null && root2 != null) || (root1 != null && root2 == null))
+			return false;
+		if (root1.getData() == root2.getData()) {
+			return checkIfMirror(root1.getLeft(), root2.getRight()) || checkIfMirror(root1.getRight(), root2.getLeft());
+		} else
+			return false;
+	}
 }
