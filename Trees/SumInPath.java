01c8bc142d6e3ff63ec07c77d9e1f210434e8a1f
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-03 14:44:19 -0400

    sum of elements of tree

--- Trees/SumInPath.java
+++ Trees/SumInPath.java
@@ -3,12 +3,21 @@ package Algorithms;
 import Implementation.BinaryTreeNode;
 
 public class SumInPath {
-	public boolean hasPathSum(BinaryTreeNode root, int sum) {
+	public static boolean hasPathSum(BinaryTreeNode root, int sum) {
 		if (root == null)
 			return false;
 		if (root.left == null && root.right == null && root.data == sum)
 			return true;
 		else
 			return hasPathSum(root.left, sum - root.data) || hasPathSum(root.right, sum - root.data);
+	}
+
+	public static int sum(BinaryTreeNode root, int sum) {
+		if (root == null)
+			return 0;
+		else {
+			sum = root.getData() + sum(root.getLeft(), sum) + sum(root.getRight(), sum);
+		}
+		return sum;
 	}
 }
