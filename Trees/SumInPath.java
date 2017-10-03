119ea5626cb0c7fd062aa48b02c8b5c827599b66
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-03 14:54:55 -0400

    sum of elements of tree iteratively

--- Trees/SumInPath.java
+++ Trees/SumInPath.java
@@ -1,6 +1,6 @@
 package Algorithms;
 
-import Implementation.BinaryTreeNode;
+import Implementation.*;
 
 public class SumInPath {
 	public static boolean hasPathSum(BinaryTreeNode root, int sum) {
@@ -17,6 +17,23 @@ public class SumInPath {
 			return 0;
 		else {
 			sum = root.getData() + sum(root.getLeft(), sum) + sum(root.getRight(), sum);
+		}
+		return sum;
+	}
+
+	public static int ite_sum(BinaryTreeNode root) {
+		if (root == null)
+			return 0;
+		int sum = 0;
+		Q_DynArray<BinaryTreeNode> q = new Q_DynArray<BinaryTreeNode>();
+		q.Enqueue(root);
+		while (!q.isEmpty()) {
+			BinaryTreeNode temp = q.Dequeue();
+			sum = sum + temp.getData();
+			if (temp.getLeft() != null)
+				q.Enqueue(temp.getLeft());
+			if (temp.getRight() != null)
+				q.Enqueue(temp.getRight());
 		}
 		return sum;
 	}
