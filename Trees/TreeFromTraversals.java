3807b4e1d0fddc33de67c38fb6a1e8ae2fdf4279
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-05 22:19:53 -0400

    construct a tree from inorder and preorder traversal

--- Trees/TreeFromTraversals.java
+++ Trees/TreeFromTraversals.java
@@ -4,11 +4,26 @@ import Implementation.*;
 
 public class TreeFromTraversals {
 	public BinaryTreeNode fromInorderPreorder(int[] inorder, int[] preorder) {
-		//construct a tree from inorder and preorder traversals
+		// construct a tree from inorder and preorder traversals
 		BinaryTreeNode root = new BinaryTreeNode(1);
-		// inorder:  D B E A F C
+		// inorder: D B E A F C
 		// preorder: A B D E C F
-		
+
 		return root;
+	}
+
+	private BinaryTreeNode formTreeInPre(int ini, int inj, int prei, int[] inorder, int[] preorder) {
+		BinaryTreeNode root = new BinaryTreeNode(preorder[prei]);
+		int i = ini;
+		for (; i < inj; i++) {
+			if (inorder[i] == preorder[prei])
+				break;
+		}
+		if(ini==inj)  return root;
+		else {
+			root.left = formTreeInPre(ini, i - 1, prei + 1, inorder, preorder);
+			root.right = formTreeInPre(i + 1, inj, prei + i-ini, inorder, preorder);
+			return root;
+		}//ababbbbcbcbqqqqqq
 	}
 }
