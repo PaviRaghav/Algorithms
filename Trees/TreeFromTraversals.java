1147679fc4251ea81b3a0d4049b40fafd538d1c8
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-06 12:55:52 -0400

    construct a tree from inorder and preorder traversal

--- Trees/TreeFromTraversals.java
+++ Trees/TreeFromTraversals.java
@@ -5,25 +5,31 @@ import Implementation.*;
 public class TreeFromTraversals {
 	public BinaryTreeNode fromInorderPreorder(int[] inorder, int[] preorder) {
 		// construct a tree from inorder and preorder traversals
-		BinaryTreeNode root = new BinaryTreeNode(1);
+		BinaryTreeNode root = formTreeInPre(0, inorder.length, 0, inorder, preorder);
 		// inorder: D B E A F C
 		// preorder: A B D E C F
-
 		return root;
 	}
 
 	private BinaryTreeNode formTreeInPre(int ini, int inj, int prei, int[] inorder, int[] preorder) {
 		BinaryTreeNode root = new BinaryTreeNode(preorder[prei]);
-		int i = ini;
-		for (; i < inj; i++) {
-			if (inorder[i] == preorder[prei])
-				break;
-		}
-		if(ini==inj)  return root;
-		else {
-			root.left = formTreeInPre(ini, i - 1, prei + 1, inorder, preorder);
-			root.right = formTreeInPre(i + 1, inj, prei + i-ini, inorder, preorder);
+		if (ini == inj && inorder[ini] == preorder[prei])
 			return root;
-		}//ababbbbcbcbqqqqqq
+		else {
+			int i = ini;
+			for (; i < inj; i++) {
+				if (inorder[i] == preorder[prei])
+					break;
+			}
+			if (i - 1 >= ini && prei + 1 < preorder.length)
+				root.left = formTreeInPre(ini, i - 1, prei + 1, inorder, preorder);
+			else
+				root.left = null;
+			if (inj >= i + 1 && prei + 1 + i - ini < preorder.length)
+				root.right = formTreeInPre(i + 1, inj, prei + 1 + i - ini, inorder, preorder);
+			else
+				root.right = null;
+			return root;
+		} // ababbbbcbcbqqqqqq
 	}
 }
