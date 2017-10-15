a6b63ac30e689f5dbe3165da7b61dad9f86dbd7c
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-14 21:17:08 -0400

    graph DFS

--- SetsAndGraphs/DFS.java
+++ SetsAndGraphs/DFS.java
@@ -1,5 +1,9 @@
 package Algorithms;
 
+import java.util.LinkedList;
+
+import Implementation.Q_DynArray;
+
 /**
  * depth-first search
  * 
@@ -7,5 +11,20 @@ package Algorithms;
  *
  */
 public class DFS {
+	private LinkedList<Integer>[] adj;
+	private int size;
+	boolean[] visited;
 
+	public void Graph_AdjList(int i) {
+		this.size = i;
+		this.adj = new LinkedList[this.size];
+	}
+
+	public void add(int a, int b) {
+		this.adj[a].add(b);
+	}
+
+	public void bfs(int a) {
+		
+	}
 }
