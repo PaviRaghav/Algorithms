1c55fdca8f0d91493f4d766be48b3af8fbd9a15d
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-21 12:24:07 -0400

    implementation of heap using array

--- Heaps/Heap.java
+++ Heaps/Heap.java
@@ -11,4 +11,29 @@ public class Heap {
 		this.heap = new int[this.maxCapacity];
 	}
 
+	public Heap(int[] A) {
+		this.maxCapacity = A.length;
+		this.heap = A;
+		heapify(A);
+	}
+
+	public void heapify(int[] A) {
+		//given an array, construct a heap
+		int i = A.length-1;
+		while (i > 0 && i > (A.length - 1) / 2) {
+			percolateDown(i);
+			i--;
+		}
+	}
+
+	private void percolateDown(int i) {
+		// given an index i, maintain the heap property by moving that element
+		int max = (this.heap[i] > this.heap[i - 1]) ? i : i - 1;
+		if (this.heap[max] > this.heap[(i - 1) / 2]) {
+			int temp = this.heap[max];
+			this.heap[max] = this.heap[(i - 1) / 2];
+			this.heap[(i - 1) / 2] = temp;
+			percolateDown((i - 1) / 2);
+		}
+	}
 }
