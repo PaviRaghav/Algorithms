f337f35c8a4fface81cd5da833c2821f7a0313b5
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-24 10:57:47 -0400

    DP LIS

---
+++ DP/LongestIncreasingSubseq.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+public class LongestIncreasingSubseq {
+	static int max_ref;
+
+	static int _lis(int arr[], int n) {
+		// base case
+		System.out.print(2 + " ");
+		if (n == 1) {System.out.print(1 + " ");
+			return 1; }
+		int res, max_ending_here = 1;
+		for (int i = 1; i < n; i++) {
+			res = _lis(arr, i);
+			System.out.println();
+			if (arr[i - 1] < arr[n - 1] && res + 1 > max_ending_here)
+				max_ending_here = res + 1;
+		}
+		if (max_ref < max_ending_here)
+			max_ref = max_ending_here;
+		return max_ending_here;
+	}
+
+	static int lis(int arr[], int n) {
+		max_ref = 1;
+		_lis(arr, n);
+		return max_ref;
+	}
+
+	public static void main(String args[]) {
+		int arr[] = { 10, 22, 9, 33,88};
+		int n = arr.length;
+		System.out.println(lis(arr, n));
+	}
+}
