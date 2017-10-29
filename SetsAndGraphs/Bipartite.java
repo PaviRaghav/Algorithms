16e3eda6420cecbbcb5f62aba599dc94f0f2b896
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-10-29 19:34:33 -0400

    bipartite graph

--- SetsAndGraphs/Bipartite.java
+++ SetsAndGraphs/Bipartite.java
@@ -9,7 +9,7 @@ public class Bipartite {
 		Unvisited, Red, Blue
 	}
 
-	private HashMap<Integer, BIPARTITECOLOR> bipartiteColor = null;
+	public HashMap<Integer, BIPARTITECOLOR> bipartiteColor = null;
 
 	public Bipartite(int[] nodes) {
 		adjList = new HashMap<Integer, LinkedList<Integer>>();
@@ -28,12 +28,45 @@ public class Bipartite {
 		}
 	}
 
-	public boolean isBipartiteBipartite(int source) {
+	public boolean isBipartite(int source) {
 		initializeBipartite();
 		Queue<Integer> q = new LinkedList<Integer>();
 		q.add(source);
 		bipartiteColor.put(source, BIPARTITECOLOR.Red);
 		return checkIfBipartite(q);
+	}
+
+	public boolean isBipartite() {
+		initializeBipartite();
+		Queue<Integer> q = new LinkedList<Integer>();
+		bipartiteColor.put(0, BIPARTITECOLOR.Red);
+		for (int i : adjList.keySet()) {
+			q.add(i);
+			if (!checkIfGraphBipartite(q))
+				return false;
+		}
+		return true;
+	}
+
+	public boolean checkIfGraphBipartite(Queue<Integer> q) {
+		if (!q.isEmpty()) {
+			int temp = q.remove();
+			LinkedList<Integer> neighbors = adjList.get(temp);
+			BIPARTITECOLOR color = bipartiteColor.get(temp);
+			for (int neighbor : neighbors) {
+				if (bipartiteColor.get(neighbor) == BIPARTITECOLOR.Unvisited) {
+					if (color == BIPARTITECOLOR.Red)
+						bipartiteColor.put(neighbor, BIPARTITECOLOR.Blue);
+					else
+						bipartiteColor.put(neighbor, BIPARTITECOLOR.Red);
+				} else {
+					if (bipartiteColor.get(neighbor) == color)
+						return false;
+				}
+			}
+			checkIfGraphBipartite(q);
+		}
+		return true;
 	}
 
 	public boolean checkIfBipartite(Queue<Integer> q) {
@@ -58,23 +91,27 @@ public class Bipartite {
 	}
 
 	public static void main(String[] args) {
-		int node[] = { 0, 1, 2, 3, 4 };
+		int node[] = { 0, 1, 2, 3, 4, 5 };
 		Bipartite grp = new Bipartite(node);
- 
+
 		grp.addEdge(0, 1);
-		grp.addEdge(0, 4);
- 
+		grp.addEdge(0, 5);
+
 		grp.addEdge(1, 0);
 		grp.addEdge(1, 2);
- 
+
 		grp.addEdge(2, 1);
 		grp.addEdge(2, 3);
- 
+
 		grp.addEdge(3, 2);
 		grp.addEdge(3, 4);
- 
-		grp.addEdge(4, 0);
+
+		grp.addEdge(4, 5);
 		grp.addEdge(4, 3);
- System.out.println(grp.isBipartiteBipartite(0));
+		
+		grp.addEdge(5, 0);
+		grp.addEdge(5, 4);
+		
+		System.out.println(grp.isBipartite());
 	}
 }
