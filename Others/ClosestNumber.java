9390a5a97966291df689c7e28fade6689bbbcc75
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-12-31 23:25:40 -0800

    closest number

--- Others/ClosestNumber.java
+++ Others/ClosestNumber.java
@@ -1,9 +1,18 @@
 /**
- * Closest number with same weight.
- * weight = number of 1's in the binary representation of the number
+ * Closest number with same weight. weight = number of 1's in the binary
+ * representation of the number
+ * 
  * @author pavithraraghavan
  *
  */
 public class ClosestNumber {
-
+	public long closestIntSameWeight(long x) {
+		// assume: input x does not contain all 1's or all 0's
+		for (int i = 0; i < 63; i++) {
+			if (((x >>> i) & 1) != ((x >>> i + 1) & 1)) {
+				x ^= (1L << i) | (1L << (i + 1));
+			}
+		}
+		return x;
+	}
 }
