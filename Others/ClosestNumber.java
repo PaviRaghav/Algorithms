da6f61dc25b0e2651ca35077cefed0132dbb0688
PaviRaghav <pavithra.raghavanms@gmail.com>
2017-12-31 23:19:38 -0800

    closest number

---
+++ Others/ClosestNumber.java
@@ -0,0 +1,9 @@
+/**
+ * Closest number with same weight.
+ * weight = number of 1's in the binary representation of the number
+ * @author pavithraraghavan
+ *
+ */
+public class ClosestNumber {
+
+}
