fa66cd91ce6cd141d6ee87f6a7b238c8700ee54a
Pavithra Raghavan <pavithra.raghavanms@gmail.com>
2018-03-03 23:13:26 -0500

    update print tree path

--- Trees/PrintTreePath.java
+++ Trees/PrintTreePath.java
@@ -1,6 +1,7 @@
 package Algorithms;
 
 import Implementation.*;
+import java.util.*;
 
 public class PrintTreePath {
 	public static void printPaths(BinaryTreeNode root, int[] path, int pathLen) {
@@ -18,7 +19,42 @@ public class PrintTreePath {
 
 	private static void printArray(int[] ints, int len) {
 		for (int i = 0; i < len; i++)
-			System.out.println(ints[i] + " ");
+			System.out.print(ints[i] + " ");
 		System.out.println();
+	}
+	
+	public void roottoleaf(BinaryTreeNode root, Deque<BinaryTreeNode> q) {
+		if(root!=null) q.addFirst(root);
+		if(root.left==null && root.right==null)
+			q = print_nodes(q);
+		else {
+			if(root.left!=null) 
+				roottoleaf(root.left,q);
+			if(root.right!=null) 
+				roottoleaf(root.right,q);
+		}
+		if(!q.isEmpty()) q.removeFirst();
+	}
+	private Deque<BinaryTreeNode> print_nodes(Deque<BinaryTreeNode> q){
+		BinaryTreeNode temp = q.peekLast();
+		do {
+			System.out.print(q.peekLast().data + " ");
+			q.addFirst(q.removeLast());
+		} while(q.peekLast()!=temp);
+		System.out.println();
+		return q;
+	}
+	
+	public static void main(String[] args) {
+		BinaryTreeNode root = new BinaryTreeNode(1);
+		root.left = new BinaryTreeNode(2);
+		root.right = new BinaryTreeNode(3);
+		root.left.left = new BinaryTreeNode(4);
+		root.left.right = new BinaryTreeNode(5);
+		root.left.right.left = new BinaryTreeNode(6);
+		root.left.right.left.left = new BinaryTreeNode(7);
+		PrintTreePath obj = new PrintTreePath();
+		Deque<BinaryTreeNode> q = new LinkedList<BinaryTreeNode>();
+		obj.roottoleaf(root, q);
 	}
 }
